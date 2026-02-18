2db26282fab1847cd16c22566d152a99e8c429eb
HOUETO Stanislas Fructueux <houetostan@gmail.com>
2026-02-18 09:26:11 +0100

    feat: Mise en place de la structure de base des dossiers et fichier du projet + requirements de bases + script bash d'initialisation

---
+++ init.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if [$ENV_EVALIA == "PROD"]; then
+  python3 -m pytest -v tests/APITesting.py -o cache_dir=/tmp &&
+    gunicorn -w 5 -b '0.0.0.0:9000' 'app:app'   \
+    --log-level debug \
+    --access-logfile - \
+    --error-logfile - \
+    --log-file - \
+    --capture-output \
+    --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
+    --enable-stdio-inheritance &
+  wait
+else
+  python3 -m pytest -v tests/APITesting.py -o cache_dir=/tmp && python3 app.py
+fi
