e647f760d84c6f6116f8773ba3e2ac76dabcf5b1
HOUETO Stanislas Fructueux <houetostan@gmail.com>
2026-02-19 09:41:01 +0100

    Le login et sa documentation

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,6 +1,10 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flasgger import swag_from
+from flask_jwt_extended import create_access_token
+from flask_bcrypt import bcrypt
+
+from models.user import User
 
 user_bp = Blueprint("users", __name__)
 @user_bp.get("/auth/user-info/<username>")
@@ -10,3 +14,20 @@ def username_informations(username: str):
     # pour récupérer le user 
     return jsonify({'username': f'{username}', 'name': "stanislas HOUETO"}), 200
 
+
+@user_bp.route("/auth/login", methods=['POST'])
+@swag_from("/app/docs/auth/login.yaml")
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    print("Les data : ", username, password)
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.checkpw(password, user.password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'message': 'Login success', 'access_token': access_token}), 200
+    else:
+        return jsonify({'message': 'Login failed'}), 401
