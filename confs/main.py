c1ae71d66e3b2b5fd01b94ad46e46e290ea6bbfc
HOUETO Stanislas Fructueux <houetostan@gmail.com>
2026-02-19 09:26:03 +0100

    feat: Mise en place de jwt et sa configuration

--- confs/main.py
+++ confs/main.py
@@ -4,11 +4,13 @@ from flask_migrate import Migrate
 from os import environ
 from sqlalchemy.orm import DeclarativeBase
 
+from flask_jwt_extended import JWTManager, create_access_token
+
 from confs.db import Database
 
 app = Flask(__name__)
 
-####### les configs de la DB
+####### les configs de la DB #########
 class Base(DeclarativeBase):
     pass
 
@@ -31,4 +33,11 @@ _db_conn = Database()
 
 # Récupérer les instance de connexion ici si voulu
 _db = _db_conn.get_instance()
+######## Fin configs DB #########
 
+
+##### LE JWT ########
+app.config["SECRET_KEY"] = environ.get("APP_SECRET_KEY")
+app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY")
+app.config['JWT_TOKEN_LOCATION'] = ['headers']
+JWTManager(app)
--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy.schema import Column
+from sqlalchemy.types import Boolean
 from confs.main import db
 
 
@@ -12,6 +13,7 @@ class User(db.Model):
     password = Column(db.String, nullable=False)
     username = Column(db.String(50), nullable=False)
     email = Column(db.String(255), nullable=False)
+    is_active = Column(Boolean(), default=True)
 
     def __repr__(self) -> str:
         return f"<User: {self.name} | {self.username}>"
