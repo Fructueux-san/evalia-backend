9c5904ceae3e24fddbd543fcec5c2f65a5ddd1d8
HOUETO Stanislas Fructueux <houetostan@gmail.com>
2026-02-20 01:18:07 +0100

    feat: ajout du fichier de blueprint pour les évaluations ainsi qu'un service de cache redis

--- blueprints/evaluation.py
+++ blueprints/evaluation.py
@@ -1,3 +1,21 @@
 # Endpoint concernant l'évalutation d'un modèle
 
 from flask import Blueprint, request, jsonify
+
+eval_bp = Blueprint("evaluation", __name__)
+
+@eval_bp.route("/eval/all-type", methods=["GET"])
+def les_type_d_evaluation_existants():
+    raise NotImplementedError()
+    #return jsonify({}), 200
+
+@eval_bp.route("/eval", methods=["POST"])
+def evaluation_model():
+    raise NotImplementedError
+
+
+@eval_bp.route("/eval/status/<model_id>")
+def status_d_evaluation():
+    raise NotImplementedError
+    
+    
