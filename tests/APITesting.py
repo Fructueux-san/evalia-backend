a1e3da17aec1e6309b4013618160464b691d9962
HOUETO Stanislas Fructueux <houetostan@gmail.com>
2026-02-28 01:38:39 +0100

    fix: retrait du tests de checking des infos utilisateurs (empêche le démarrage alors que la fonctionnalité a été retiré à cet instant T

--- tests/APITesting.py
+++ tests/APITesting.py
@@ -29,12 +29,12 @@ class APITesting(unittest.TestCase):
     def tearDownClass(cls) -> None:
         os.environ.pop("TESTING")
 
-    def test__recuperer_les_informations_d_un_user(self):
-        response = self.client.get(
-            "/api/auth/user-info/stan-hto",
-            headers=self._base_header
-        )
-        if response.status_code != 200:
-            logging.info(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 200) # Ou bien assertIn
+    # def test__recuperer_les_informations_d_un_user(self):
+    #     response = self.client.get(
+    #         "/api/auth/user-info/stan-hto",
+    #         headers=self._base_header
+    #     )
+    #     if response.status_code != 200:
+    #         logging.info(response.get_data(as_text=True))
+    #     self.assertEqual(response.status_code, 200) # Ou bien assertIn
 
